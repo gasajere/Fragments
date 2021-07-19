ae6f72491f7c07c9a2b93ba6d70fd62e1b889a4b
gasajere <gasajere@gmail.com>
2021-07-19 17:55:54 +0100

    Initial commit

---
+++ app/src/main/java/com/terence/fragments/AFragment.kt
@@ -0,0 +1,43 @@
+package com.terence.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.terence.fragments.databinding.FragmentABinding
+
+
+class AFragment : Fragment() {
+    private var _binding: FragmentABinding? = null
+    private val binding get() = _binding!!
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentABinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.moveBtn.setOnClickListener {
+           val activity = requireActivity() as MainActivity
+            activity.navigate(BFragment())
+            }
+        }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+
+}
